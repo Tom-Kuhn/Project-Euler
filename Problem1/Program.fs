ac5372f3be3ab8e764106bd9291a39ca51add94b
Tom Kuhn <tom.c.kuhn@gmail.com>
2014-09-09 11:22:11 +0100

    Initial version of problem 1 & 2

---
+++ Problem1/Program.fs
@@ -0,0 +1,12 @@
+﻿
+let isNumberInteresting x = if (x % 3 = 0 || x % 5 = 0) then x else 0    
+
+[<EntryPoint>]
+let main argv = 
+    
+    let multiples = [1 .. 999] |> List.map isNumberInteresting
+    let sum = multiples |> List.sum
+
+    printfn "%i" sum
+
+    0 // return an integer exit code
---
+++ Problem2/Program.fs
@@ -0,0 +1,10 @@
+﻿[<EntryPoint>]
+let main argv = 
+    let takeEvenNumber y = if y &&& 0x1 = 1 then 0 else y;
+    
+    let fib = Seq.unfold (fun (current, next) -> Some(current, (next, current + next))) (1, 2)
+
+    let fibTotal = fib |> Seq.takeWhile(fun x-> x < 4000000 ) |> Seq.map takeEvenNumber |> Seq.sum 
+        
+    printfn "%A" fibTotal
+    0 // return an integer exit code
