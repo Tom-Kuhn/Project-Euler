a365e20f6451f13045a47f2fb9deddc6361d7b8a
Tom Kuhn <tom.c.kuhn@gmail.com>
2014-09-09 15:54:19 +0100

    Solution for Problem 3

---
+++ Problem3/Program.fs
@@ -0,0 +1,23 @@
+﻿
+let isFactor x value = (value % x) = 0L
+
+let getFactors x =  
+    let maxFactor = float >> sqrt >> int64 <| x
+    [2L .. maxFactor] |> Seq.filter (fun i -> isFactor i x)
+
+let isPrime x = 
+    let factors = getFactors x
+    let result = Seq.isEmpty factors
+    result
+
+[<EntryPoint>]
+let main argv = 
+    let target = 600851475143L;
+    
+    let factors = getFactors target     
+    let primeFactors = factors |> Seq.filter isPrime
+    
+    let largestPrime = Seq.max primeFactors   
+
+    printfn "%A" argv
+    0 // return an integer exit code
