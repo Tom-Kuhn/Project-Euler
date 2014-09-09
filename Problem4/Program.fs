150af2db22124daa3a5a01a9ec3750e214a1fdfc
Tom Kuhn <tom.c.kuhn@gmail.com>
2014-09-09 16:30:31 +0100

    Added solution for Problem 4

---
+++ Problem4/Program.fs
@@ -0,0 +1,22 @@
+﻿open System
+open System.Globalization
+
+let isPallendrome x = 
+    let forward = x.ToString()
+    let backward = String(forward.ToCharArray() |> Array.rev)
+    forward = backward
+
+[<EntryPoint>]
+let main argv = 
+    let val1 = [999 .. -1 .. 900]
+    let val2 = [999 .. -1 .. 900]
+
+    let maxVal = 
+        seq { 
+            for i in val1 do
+                for j in val2 do
+                    yield i * j
+        } |>  Seq.sort |> Seq.filter isPallendrome |>Seq.max
+
+    printfn "%A" argv
+    0 // return an integer exit code
