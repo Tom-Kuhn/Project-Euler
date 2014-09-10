ef25bae32488d641a8d36d6b4bbaecf0a02ed6f4
TK <tom.c.kuhn@gmail.com>
2014-09-10 17:11:00 +0100

    Added a solution for problem 7

---
+++ Problem7/Program.fs
@@ -0,0 +1,28 @@
+﻿
+let isFactor x value = (value % x) = 0L
+
+let getFactors x =  
+    let maxFactor = float >> sqrt >> int64 <| x
+    [2L .. maxFactor] |> Seq.filter (fun i -> isFactor i x)
+
+let isPrime x = 
+    Seq.isEmpty (getFactors x)
+
+///
+/// Recursive function to determine the next prime
+///
+let rec nextPrime x =
+    if isPrime x then x
+    else nextPrime(x + 1L)
+
+[<EntryPoint>]
+let main argv = 
+    let target = 10001
+
+    let primes = Seq.unfold(fun (current) -> Some(current, (nextPrime(current + 1L)))) (1L)
+
+    let result = Seq.nth target primes
+
+    printfn "%i" result
+
+    0 // return an integer exit code
