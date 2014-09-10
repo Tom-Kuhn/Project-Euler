2e872026f62f572a2854e2181a2463e19641f99f
Tom Kuhn <tom.c.kuhn@gmail.com>
2014-09-10 12:02:57 +0100

    Added potential solution for Problem 6

---
+++ Problem6/Program.fs
@@ -0,0 +1,17 @@
+﻿// Learn more about F# at http://fsharp.net
+// See the 'F# Tutorial' project for more help.
+
+[<EntryPoint>]
+let main argv = 
+    let range = [1 .. 100]
+
+    let square x = x*x
+
+    let sumOfSquares = range |> Seq.map square |> Seq.sum
+    let squareOfSum = square (range |> Seq.sum)
+
+    let difference = squareOfSum - sumOfSquares
+
+    printfn "%i" difference
+
+    0 // return an integer exit code
