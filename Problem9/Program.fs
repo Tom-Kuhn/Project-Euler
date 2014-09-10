771a2910f7f63492fb93a9a014bd657da608b7bf
TK <tom.c.kuhn@gmail.com>
2014-09-10 20:52:59 +0100

    Improved the efficiency of the solution by removing unecessary steps

--- Problem9/Program.fs
+++ Problem9/Program.fs
@@ -1,23 +1,18 @@
 ﻿[<EntryPoint>]
 let main argv = 
+    let targetSum = 1000
 
     // Generate all possible Pythagoras triplets up to 500
     let pythagTriplets = 
         seq {
             for a in [1..500] do
                 for b in [1..500] do
-                    for c in [1..500] do
-                        if (a*a) + (b*b) = (c*c) then yield (a, b, c)
+                    let c = targetSum - a - b 
+                    if (a*a) + (b*b) = (c*c) then yield (a, b, c)
         }
 
-    // function to sum a Triplet<int, int, int>
-    let sumTriplet x = 
-        let (a, b, c) = x
-        a + b + c
-
-    // Filter all the Pythagoras triplets to only the ones that add up to 1000 and select the first
-    let solution = pythagTriplets |> Seq.filter (fun x -> sumTriplet x = 1000 )
-    let (a, b, c) = solution |> Seq.max
+    // Filter the Pythagoras triplets and select the first
+    let (a, b, c) = pythagTriplets |> Seq.max
     
     // Calculate the result
     let result = a*b*c
