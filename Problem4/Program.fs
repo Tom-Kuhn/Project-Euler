3f27e3265e79c7b2728811487a356660ed995df7
Tom Kuhn <tom.c.kuhn@gmail.com>
2014-09-10 11:21:39 +0100

    Added brute force solution to problem 5

--- Problem4/Program.fs
+++ Problem4/Program.fs
@@ -4,10 +4,11 @@ open System.Globalization
 let isPallendrome x = 
     let forward = x.ToString()
     let backward = String(forward.ToCharArray() |> Array.rev)
-    forward = backward
+    forward = backward // If the forward string == the reversed string return true
 
 [<EntryPoint>]
 let main argv = 
+    // Set the range 999 -> 900 decending, assume that the largest product will be in the 9xx * 9xx range
     let val1 = [999 .. -1 .. 900]
     let val2 = [999 .. -1 .. 900]
 
@@ -15,8 +16,8 @@ let main argv =
         seq { 
             for i in val1 do
                 for j in val2 do
-                    yield i * j
+                    yield i * j // Generate a sequence of products for all combinations of values within the two ranges above
         } |>  Seq.sort |> Seq.filter isPallendrome |>Seq.max
 
-    printfn "%A" argv
+    printfn "%i" maxVal
     0 // return an integer exit code
---
+++ Problem5/Program.fs
@@ -0,0 +1,14 @@
+﻿[<EntryPoint>]
+let main argv = 
+    let divisors = [2 .. 20]
+
+    let isDivisorsFactor x = ( divisors |> Seq.map (fun factor -> x % factor) |> Seq.sum ) = 0 // All Values in the mapped array are 0 if all the divisors are factors 
+
+    let options = [2000 .. Seq.max divisors .. 999999999] // set the list of possible options to be in multiples of the max divisor
+    let result = match List.tryFind isDivisorsFactor options with
+        | Some value -> value
+        | None -> -1 
+        
+    printfn "%i" result
+
+    0 // return an integer exit code
