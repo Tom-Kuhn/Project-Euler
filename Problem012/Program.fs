4820faf9268850cc4ab671664cf425f2b6162d5a
Tom Kuhn <tom.c.kuhn@gmail.com>
2014-09-17 12:35:36 +0100

    Solve problem 13 & 14

---
+++ Problem012/Program.fs
@@ -0,0 +1,31 @@
+﻿let numberOfFactors n =
+    let isFactor number divisor = number % divisor = 0L
+
+    // Recursive call to count the number of divisors in a particular number
+    let rec countDivisors number divisor =
+        if isFactor number divisor then // If the divisor is 
+            let multiple, count = countDivisors (number/divisor) divisor
+            multiple, count + 1L
+        else
+            number, 0L // No divisors remaining for number
+ 
+    // Recursive function to collate all the possible factors 
+    let rec collate number divisor =
+        if number < divisor then 
+            1L // Stop condition
+        // If the divisor is a factor, calculate the number of multiples present for this divisor
+        elif isFactor number divisor then
+            let multiple, count = countDivisors number divisor
+            (count + 1L) * (collate multiple divisor) // Return the number of factors for this divisor
+        else
+            collate number (divisor + 1L) // Iterate onto the next possible divisor
+    
+    collate n 2L // Start the recursive calls to find the number of divisors
+
+[<EntryPoint>]
+let main argv = 
+    let triangleNumbers = Seq.initInfinite (fun n -> (((int64 n) * ((int64 n) + 1L)) / 2L))
+    let n = triangleNumbers |> Seq.find(fun x -> (numberOfFactors x) > 500L)
+    
+    printfn "%i" n
+    0 // return an integer exit code
---
+++ Problem014/Program.fs
@@ -0,0 +1,16 @@
+﻿[<EntryPoint>]
+let main argv = 
+    let collatz n =
+        Seq.unfold (
+            fun x -> 
+                if x < 1L then None // Stop condition
+                elif x = 1L then Some(1L, -1L) // Special case to indicate last value reached
+                elif ( x % 2L = 0L ) then Some(x, (x / 2L))
+                else Some(x, (3L * x) + 1L)
+        ) n
+
+    let sequences = [750000L .. 999999L] |> Seq.map (fun x -> (x, Seq.length (collatz x)))
+    let (num, length) = Seq.maxBy snd sequences 
+
+    printfn "%A" num
+    0 // return an integer exit code
