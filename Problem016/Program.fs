4ab3c2762f975f18ce328e77fffb8c74d144505e
Tom Kuhn <tom.c.kuhn@gmail.com>
2014-09-29 10:29:26 +0100

    Initial thoughts on problem 16

---
+++ Problem016/Program.fs
@@ -0,0 +1,25 @@
+﻿open System
+
+[<EntryPoint>]
+let main argv = 
+    // Declare an array of digits for the multiplication LSB first e.g. lowest order digits are in index 0 and the highest order digits are in the higher indexes
+    let results = [| Array.zeroCreate 400 |]
+    Array.set results.[0] 0 2
+
+    let calculateValue index value = 
+        if index <> 0 then
+            if (results.[index-1] >= 10) then
+                ((value + ((results.[index-1]) - ((results.[index-1]) % 10 ))) * 2)
+            else
+                (value * 2)
+        else
+            // Deal with the first index seperatly as it wont have a previous value
+            (value * 2)
+    
+    for x in 1 .. 1000 do
+        Array.append (Array.mapi calculateValue results.[x - 1])       
+
+    let result = 0        
+
+    printfn "%i" result
+    0 // return an integer exit code
