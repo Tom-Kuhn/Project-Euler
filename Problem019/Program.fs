f99e051a0ad971d37011e4205604b47e20e77be7
Tom Kuhn <tom.c.kuhn@gmail.com>
2014-10-03 15:41:40 +0100

    Made the solution more concise using a filter clause

--- Problem019/Program.fs
+++ Problem019/Program.fs
@@ -2,8 +2,6 @@
 
 [<EntryPoint>]
 let main argv = 
-    let doesMonthStartOnSunday year month = if (new DateTime(year, month, 1)).DayOfWeek = DayOfWeek.Sunday then 1 else 0
-    let result = {1901 .. 2000} |> Seq.map(fun y -> {1 .. 12} |> Seq.map(fun m -> doesMonthStartOnSunday y m ) |> Seq.sum ) |> Seq.sum
-
+    let result = {1901 .. 2000} |> Seq.map(fun y -> {1 .. 12} |> Seq.filter(fun m -> (new DateTime(y, m, 1)).DayOfWeek = DayOfWeek.Sunday) |> Seq.length) |> Seq.sum
     printfn "%i" result
     0 // return an integer exit code
