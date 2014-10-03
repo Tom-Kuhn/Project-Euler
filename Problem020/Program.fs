8fe640e60682f49322d32bdbdcd061a41220e978
Tom Kuhn <tom.c.kuhn@gmail.com>
2014-10-03 16:14:37 +0100

    Solve problem 20

---
+++ Problem020/Program.fs
@@ -0,0 +1,10 @@
+﻿open System
+
+[<EntryPoint>]
+let main argv = 
+    let factorial n = [1I .. n] |> Seq.reduce ( * )
+
+    let result = (factorial 100I).ToString() |> Seq.map(fun x-> Int32.Parse(x.ToString())) |> Seq.sum
+
+    printf "%A" result
+    0
