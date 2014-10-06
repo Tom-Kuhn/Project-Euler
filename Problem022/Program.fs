0144d8ba81b4e9c9faf5f7709e63c16269df5c48
Tom Kuhn <tom.c.kuhn@gmail.com>
2014-10-06 14:26:46 +0100

    Condensed the solution code

--- Problem022/Program.fs
+++ Problem022/Program.fs
@@ -1,11 +1,9 @@
 ﻿open System
 open System.IO
 
-let getData (path: string) = Array.sort(String.Join(",", File.ReadAllLines(path)).Split(',') |> Array.map (fun x -> x.Replace("\"", "")))
-
 [<EntryPoint>]
 let main argv = 
+    let getData (path: string) = Array.sort(String.Join(",", File.ReadAllLines(path)).Split(',') |> Array.map (fun x -> x.Replace("\"", "")))
     let result = getData "names.txt"|> Seq.map(fun x -> x.ToCharArray() |> Seq.map(fun y -> (int y) - 64) |> Seq.sum) |> Seq.mapi (fun i x -> (i+1) * x) |> Seq.sum
-    
     printfn "%i" result
     0 // return an integer exit code
